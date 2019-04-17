af7acd89164c5253063e02e9ee5225de0fb8d483
Demon Singur <demonsingur@gmail.com>
2019-04-17 16:29:09 +0800

    sdm710-common: Import fingerprint HIDL
    Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9

--- sdm710.mk
+++ sdm710.mk
@@ -177,10 +177,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.2-service.clearkey
 
-# Fingerprint
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm710.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm710.rc
-
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0-impl-qti \
