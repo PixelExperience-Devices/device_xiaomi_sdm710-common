ba44469f708d4a2104c5dafc8ab31307c7a7db6f
Alex <alexeydomoratsky1@gmail.com>
2020-01-21 20:30:30 +0300

    sdm710-common: Exclude lineage-sdk overlays from RRO

--- sdm710.mk
+++ sdm710.mk
@@ -16,8 +16,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
