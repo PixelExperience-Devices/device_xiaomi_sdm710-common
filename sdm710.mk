f6c1047971899dce014e40d51461680a756b8579
Bruno Martins <bgcngm@gmail.com>
2020-01-26 00:01:13 +0000

    sdm710-common: Update destination path for HotwordEnrollment permission file
    HotwordEnrollment has been moved to /product in Q.
    
    Change-Id: Ifc30940dcfc6ef236ed507ddfec1189b7e4f547b

--- sdm710.mk
+++ sdm710.mk
@@ -66,7 +66,7 @@ PRODUCT_COPY_FILES += \
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
-	
+
 # AOT Preload
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
@@ -138,7 +138,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
-	
+
 # Codec2
 PRODUCT_PACKAGES += \
     libcodec2_vndk.vendor \
@@ -215,7 +215,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
 PRODUCT_PACKAGES += \
@@ -244,7 +244,7 @@ PRODUCT_COPY_FILES += \
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-	
+
 # IPA
 PRODUCT_PACKAGES += \
     ipacm \
