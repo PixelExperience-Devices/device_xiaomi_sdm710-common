18959cddecb70a8bbc90170061db74c048f7c9d7
YumeMichi <do4suki@gmail.com>
2020-02-19 04:24:29 +0800

    sdm710-common: Switch to OSS audio HAL
    Change-Id: Ib5cf3b1e971082c56ac7c8cfe0a3b8980d3986bc
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -79,11 +79,20 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
+    audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
+    libbatterylistener \
+    libcomprcapture \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
     libtinycompress \
     libvolumelistener \
     tinymix
