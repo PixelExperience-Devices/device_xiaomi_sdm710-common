9dfe4f8b2d5df65423f39eb16232e38f13525c02
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    sdm710-common: Build and enable fastbootd mode
    Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -188,6 +188,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0 \
