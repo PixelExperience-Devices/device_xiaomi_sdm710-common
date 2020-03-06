5bd64b92ce9374030fd1ff560155af9be1323a41
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-03-06 13:40:32 -0300

    sdm710-common: Build LiveDisplay
    * Let's-a-go
    * This reverts commit f3b58a8744589a0b21e072c7257b373bc22e7792.
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- sdm710.mk
+++ sdm710.mk
@@ -254,6 +254,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm710
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
