886adad945883f53ffad1389e42dd168856d38b6
solidv <pedroclaro2@gmail.com>
2021-10-27 19:27:57 +0100

    Revert "sdm710-common: Drop no longer existing BluetoothQti"
    This reverts commit 3f8e6e9683fe6d01c62852571a473f435eebbb7d.

--- sdm710.mk
+++ sdm710.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
+    BluetoothQti \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
