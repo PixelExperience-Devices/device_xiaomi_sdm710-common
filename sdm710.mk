c1ef495f87034cf8593eacbd9e32f22dd6e4fe69
solidv <pedroclaro2@gmail.com>
2021-11-14 21:40:42 +0000

    sdm710-common: build keymaster 4.1

--- sdm710.mk
+++ sdm710.mk
@@ -262,8 +262,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0 \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@4.1.vendor
 
 # Lights
 PRODUCT_PACKAGES += \
