1ad89de4c5d1a026769f3288f0a6283e30df9ce7
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    sdm710-common: specify androidboot.boot_devices
    so that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,7 @@ BOARD_KERNEL_CMDLINE += ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7c4000.sdhci
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
